9ff1e3be8e3c44fbcd8284043e66c3786f5be6d7
Dorian Mendoza <dorian.mendoza@csiciber.com>
2021-01-21 17:20:35 -0600

    commit dia 21/01/2021

--- src/context/ComponenteContext.js
+++ src/context/ComponenteContext.js
@@ -6,26 +6,26 @@ export const ComponenteContext = createContext()
 // Provider es donde se encuentran las funciones y state
 const ComponenteProvider = (props) => {
 
-    //const componente_ls = JSON.parse(localStorage.getItem('componente'))  
+    //const componente_ls = JSON.parse(localStorage.getItem('componente'))
 
     const resultado = JSON.parse(localStorage.getItem('componente'))
     let nivel_acceso = 0
     let numero_ventana = 0
     let numero_componente = 0
-    
+
     if(resultado){
         nivel_acceso = resultado.nivel_acceso
         numero_ventana = resultado.numero_ventana
         numero_componente = resultado.numero_componente
-    }  
-  
+    }
+
     const [ componentecontx, guardarComponenteContx ] = useState({
         nivel_acceso: nivel_acceso,
         numero_ventana: numero_ventana,
         numero_componente: numero_componente
-    })    
+    })
 
-    return ( 
+    return (
         <ComponenteContext.Provider
             value={{
                 componentecontx,
